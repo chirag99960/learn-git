27d34a1905dd1c265b7f052dea4e6abedaa24557
chirag <chiragchandak99960@gmail.com>
2023-06-07 09:20:14 +0530

    new python files

---
+++ chirag.py
@@ -0,0 +1,6 @@
+# this is the comment in the chirag branch.      
+a=eval(input("enter the 1st no.="))
+b=eval(input("enter the 2st no.="))
+sum=a+b
+print(sum)
+print("this is the sum of two numbers.") 
